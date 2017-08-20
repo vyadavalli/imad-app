71ffd9ff34b29062b51ae2a964b91c007ae8d299
vyadavalli <31188772+vyadavalli@users.noreply.github.com>
2017-08-21 00:13:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("Article One"));
+  res.send("Article One");
 });
 
 app.get('/ui/style.css', function (req, res) {
